2e210b0ed483807eee89d61c7a1f99f1e2f6abcf
cnangel <cnangel@gmail.com>
2009-09-28 12:02:55 +0800

    add browserStatusOverlay.pm

---
+++ lib/Label/View/browserStatusOverlay.pm
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+
+package Label::View::browserStatusOverlay;
+use base 'XUL::App::View::Base';
+use Template::Declare::Tags
+    #'HTML' => { namespace => 'html' },  # HTML namespace support
+    'XUL';
+
+template main => sub {
+    show 'header';  # from XUL::App::View::Base
+    overlay {
+        attr {
+            id => "label-browserstatusoverlay",
+            xmlns => $::XUL_NAME_SPACE,
+            #'xmlns:html' => $::HTML_NAME_SPACE,  # HTML namespace support
+
+        }
+        # Add your elements here...
+
+    }
+};
+
+1;
